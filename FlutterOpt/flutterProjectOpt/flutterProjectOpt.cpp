0c923fb0a8c5eaf20eb63cfb40328c4ddd0c1e30
reigadegr <2722688642@qq.com>
2024-03-11 16:57:59 +0800

    add flutter tools

---
+++ FlutterOpt/flutterProjectOpt/flutterProjectOpt.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+#define log(x) std::cout<<x<<std::endl;
+
+auto init_filesystem() {
+    if (!fs::exists("pubspec.yaml")) {
+        log("Not flutter project!");
+        return false;
+    }
+    std::string directoryPath = "./assets/images";
+
+    if (fs::exists(directoryPath)) {
+        std::cout << "Directory already exists: " << directoryPath << std::endl;
+        return true;
+    }
+    if (fs::create_directories(directoryPath)) {
+        std::cout << "Directory created successfully: " << directoryPath << std::endl;
+        return true;
+    }
+    std::cerr << "Failed to create directory: " << directoryPath << std::endl;
+    return false;
+
+}
+
+int main(int argc, char **argv) {
+    init_filesystem();
+    std::cin.get();
+}
